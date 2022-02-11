ebdbe41db306e293ab8aaaa5e2b9d923a82abe63
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-02-11 10:34:21 +0900

    [ADD] MyBlog-Project

---
+++ src/test/java/week3/solo1/Solo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package week3.solo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Solo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
